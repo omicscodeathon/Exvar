d37acf888b4c163185d9937b67e7bfe3870841ed
Hiba Ben Aribi <73958439+HibaBenAribi@users.noreply.github.com>
2023-03-31 14:12:54 +0100

    update requirements + species param

--- Package/R/vizexp.R
+++ Package/R/vizexp.R
@@ -1,156 +1,175 @@
-#'  vizexp
-#'
+#' vizexp
+#' This function is a shiny app to visualize gene expression data.
 #' @param genecount Path to the gene count file
 #' @param metadata Path to the  metadata file
-#' @param species Species name ( "Homo Sapiens", "Mus Musculus","Arabidopsis Thaliana" ,
-#' "Drosophila Melanogaster", "Danio rerio", "Rattus norvegicus", or "Saccharomyces Cerevisiae" )
 #' @export
 #'
 vizexp <- function( genecount, metadata, species) {
-  
-  #load requirements
-  library(shinydashboard)
+  #request specie
+  cat(paste0("These are the species currently supported by ExpVar: \n",
+             "[1] Homo sapiens \n",
+             "[2] Mus musculus \n",
+             "[3] Arabidopsis thaliana \n",
+             "[4] Drosophila melanogaster \n",
+             "[5] Danio rerio \n",
+             "[6] Rattus norvegicus \n",
+             "[7] Saccharomyces cerevisiae \n"))
+  species <- readline("Type the number of the species that you would like to use as a reference: ")
+  #  load requirements
+  library(shinydashboard) 
   library(shiny)
   library(shinythemes)
   library(shinyWidgets)
+  library(shinycssloaders)
   library(readr)
   library(DESeq2)
-  library(dplyr)
-  library(ggplot2)
-  library(ComplexHeatmap)
-  library(clusterProfiler)
-  library(AnnotationDbi)
-  library(enrichplot)
-  library(ggnewscale)
+  library(dplyr) 
+  library(ggplot2) 
   library(tibble)
   library(readxl)
-  library(shinycssloaders)
+  library(clusterProfiler)
+  library(enrichplot)
+  # species specific requirements
+  switch(species,
+         "1"={
+           library(org.Hs.eg.db)
+           specie1 <- "org.Hs.eg.db"
+         },
+         "2"={
+           library(org.Mm.eg.db)
+           specie1 <-"org.Mm.eg.db"
+         },
+         "3"={
+           library(org.At.tair.db)
+           specie1 <-"org.At.tair.db"
+         },
+         "4"={
+           library(org.Dm.eg.db)
+           specie1 <-"org.Dm.eg.db"
+         },
+         "5"={
+           library(org.Dr.eg.db)
+           specie1 <-"org.Dr.eg.db"
+         },
+         "6"={
+           library(org.Rn.eg.db)
+           specie1 <-"org.Rn.eg.db"
+         },
+         "7"={
+           library(org.Sc.sgd.db)
+           specie1 <-"org.Sc.sgd.db"
+         }
+  )
+  #  demo data
+  genecount <-"Countdata.csv"
+  metadata <- "metadata.xlsx"
   
-  #specie
-  if (species == "Homo Sapiens"){
-    library(org.Hs.eg.db)
-    specie1 <- "org.Hs.eg.db"
-  } else if (species == "Mus Musculus"){
-    library(org.Mm.eg.db)
-    specie1 <-"org.Mm.eg.db"
-  } else if (species == "Arabidopsis Thaliana"){
-    library(org.At.tair.db)
-    specie1 <-"org.At.tair.db"
-  } else if (species == "Drosophila Melanogaster"){
-    library(org.Dm.eg.db)
-    specie1 <-"org.Dm.eg.db"
-  } else if (species == "Danio rerio"){
-    library(org.Dr.eg.db)
-    specie1 <-"org.Dr.eg.db"
-  } else if (species == "Rattus_norvegicus"){
-    library(org.Rn.eg.db)
-    specie1 <-"org.Rn.eg.db"
-  } else if (species == "Saccharomyces Cerevisiae"){
-    library(org.Sc.sgd.db)
-    specie1 <-"org.Sc.sgd.db"
-  }
-  
-  #prepare files
-  #count data
-  countdata <- read_csv(genecount)
-  countdata = as.data.frame(countdata)
+  ## read input files + process files      
+  # Note the genecount file must contain only one column of the genes symbol named "genes"
   # Remove duplicates based on gene column
-  countdata <- countdata[!duplicated(countdata$gene), ]
-  countdata<- na.omit(countdata)
+  countdata <- read.csv(genecount) %>%
+    distinct(gene, .keep_all = TRUE) %>%
+    na.omit() %>%
+    as_tibble() %>%
+    column_to_rownames("gene") %>%
+    as.data.frame() %>%
+    select(-1)
   
-  rownames(countdata) = countdata$gene   #first column name
-  countdata  = countdata [,-c(1)]
-  
-  #metadata
-  metadata <- read_excel("metadata.xlsx")
-  coldata <-as.data.frame( na.omit(metadata))
-  rownames(coldata) = coldata$Sample
-  
-  # #run app
-  ui <- bootstrapPage(
-    navbarPage(theme = shinytheme("flatly"), collapsible = TRUE,
-               HTML('<a style="text-decoration:none;cursor:default;color:#FFFFFF;" class="active" href="#">EXPviz</a>'), id="nav",
-               windowTitle = "EXPviz",
-               tabPanel("Count Data",
-                        tabBox(title = "Before log2 transformation", width = 6,
-                               box(shinycssloaders::withSpinner(plotOutput("read_count1")))),
-                        
-                        tabBox(title = "After log2 transformation", width = 6,
-                               tabPanel("Read count",
-                                        shinycssloaders::withSpinner(plotOutput("log_transformation2"))),
-                               tabPanel("density plot ",
-                                        shinycssloaders::withSpinner(plotOutput("density_plot")))
-                        )),
-               tabPanel("Expression Analysis",
-                        sidebarLayout(
-                          sidebarPanel(
+  #metadata = coldata
+  # Note the metadata file must contain a column contained the samples ids named "sample"
+  # and a column of the samples' phenotype named "factor"
+  coldata <- read_excel(metadata) %>%
+    na.omit() %>%
+    column_to_rownames("sample")
+  ##run app
+  ui <-   navbarPage(
+    theme = shinytheme("flatly"),
+    HTML('<a style="text-decoration:none;
+               cursor:default;
+                    color:white;
+                    " class="active" href="#">Visualize Gene Expression data</a>'),
+    windowTitle ="expviz",
+    tabPanel("Count Data",
+             span(shiny::tags$i(h2("Visualize gene count data : ")), style="color:#045a8d"),
+             tabBox(title = "Before log2 transformation", width = 6,
+                    box(shinycssloaders::withSpinner(plotOutput("read_count1")))),
+             
+             tabBox(title = "After log2 transformation", width = 6,
+                    tabPanel("Read count",
+                             shinycssloaders::withSpinner(plotOutput("log_transformation2"))),
+                    tabPanel("density plot ",
+                             shinycssloaders::withSpinner(plotOutput("density_plot")))
+             )),
+    tabPanel("Expression Analysis",
+             span(shiny::tags$i(h2("Perform gene expression analysis :")), style="color:#045a8d"),
+             sidebarLayout(
+               sidebarPanel(
+                 span(shiny::tags$i(h6("Define the p-value to identify the differentially expressed genes")), style="color:#045a8d"),
+                 br(),
+                 sliderInput("pval",
+                             "P_value:",
+                             min = 0,
+                             max = 1,
+                             value=0.05
+                 ),
+                 br(),
+                 span(shiny::tags$i(h6("Define the LogFC value to identify the differentially expressed genes")), style="color:#045a8d"),
+                 br(),
+                 sliderInput("log",
+                             "Log value:",
+                             min = 0,
+                             max = 5,
+                             value=2
+                 )
+               ),
+               mainPanel(
+                 tabsetPanel(
+                   tabPanel("MA Plot",
+                            shinycssloaders::withSpinner(plotOutput("ma_plot", height = 500))
                             
-                            span(shiny::tags$i(h6("Define the p-value to identify the differentially expressed genes")), style="color:#045a8d"),
-                            br(),
-                            sliderInput("pval",
-                                        "P_value:",
-                                        min = 0,
-                                        max = 1,
-                                        value=0.05
-                            ),
-                            br(),
-                            span(shiny::tags$i(h6("Define the LogFC value to identify the differentially expressed genes")), style="color:#045a8d"),
-                            br(),
-                            sliderInput("log",
-                                        "Log value:",
-                                        min = 0,
-                                        max = 5,
-                                        value=2
-                            )
-                          ),
-                          mainPanel(
-                            tabsetPanel(
-                              tabPanel("MA Plot",
-                                       shinycssloaders::withSpinner(plotOutput("ma_plot", height = 500))
-                                       
-                              ),
-                              tabPanel("PCA Plot",
-                                       shinycssloaders::withSpinner(plotOutput("pca_plot", height = 500))
-                                       
-                              ),
-                              tabPanel("Volcano Plot",
-                                       shinycssloaders::withSpinner(plotOutput("volcano_plot", height = 500))
-                              )
-                            )
-                          )
-                        )),
-               tabPanel("Ontology",
-                        
-                        sidebarLayout(
-                          sidebarPanel(
-                            pickerInput("select", label= span(shiny::tags$i(h6("Define the gene group")), style="color:#045a8d"),
-                                        
-                                        choices = c("All Differentially Expresssed Genes", "Upregulated genes", "Downregulated genes" ),
-                                        selected = c("All Differentially Expresssed Genes"),
-                                        multiple = FALSE),
-                            br(),
-                            textInput("show", label=  span(shiny::tags$i(h6("Top Ontologies")), style="color:#045a8d")
-                                      ,value = "20"),
-                            br(),
-                            pickerInput("plottype", label= span(shiny::tags$i(h6("Define the Plot type")), style="color:#045a8d"),
-                                        
-                                        choices = c("Dotplot","Barplot",  "Cnetplot" ),
-                                        multiple = FALSE),
-                            br(),
-                            pickerInput("GOtype", label= span(shiny::tags$i(h6("Define the Ontology category")), style="color:#045a8d"),
-                                        
-                                        choices = c("Biological Processes","Cellular Compenant",  "Molecular Function" ),
-                                        multiple = FALSE)
-                          ),
-                          mainPanel(
-                            shinycssloaders::withSpinner(plotOutput("ontology_plot"))
+                   ),
+                   tabPanel("PCA Plot",
+                            shinycssloaders::withSpinner(plotOutput("pca_plot", height = 500))
                             
-                          )
-                        )
+                   ),
+                   tabPanel("Volcano Plot",
+                            shinycssloaders::withSpinner(plotOutput("volcano_plot", height = 500))
+                   )
+                 )
                )
-    ))
-  
-  server <- function(input, output) {
+             )),
+    tabPanel("Ontology Analysis",
+             span(shiny::tags$i(h2("Perform gene ontology analysis")), style="color:#045a8d"),
+             sidebarLayout(
+               sidebarPanel(
+                 pickerInput("select", label= span(shiny::tags$i(h6("Define the gene group")), style="color:#045a8d"),
+                             
+                             choices = c("All Differentially Expresssed Genes", "Upregulated genes", "Downregulated genes" ),
+                             selected = c("All Differentially Expresssed Genes"),
+                             multiple = FALSE),
+                 br(),
+                 textInput("show", label=  span(shiny::tags$i(h6("Top Ontologies")), style="color:#045a8d")
+                           ,value = "20"),
+                 br(),
+                 pickerInput("plottype", label= span(shiny::tags$i(h6("Define the Plot type")), style="color:#045a8d"),
+                             
+                             choices = c("Dotplot","Barplot",  "Cnetplot" ),
+                             multiple = FALSE),
+                 br(),
+                 pickerInput("GOtype", label= span(shiny::tags$i(h6("Define the Ontology category")), style="color:#045a8d"),
+                             
+                             choices = c("Biological Processes","Cellular Compenant",  "Molecular Function" ),
+                             multiple = FALSE)
+               ),
+               mainPanel(
+                 shinycssloaders::withSpinner(plotOutput("ontology_plot"))
+                 
+               )
+             )
+    )
+  )
+  ## server
+  server <- function(input, output,session) {
     
     output$read_count1 <- renderPlot({
       par(mar=c(8,4,4,1)+0.1)
@@ -178,7 +197,7 @@ vizexp <- function( genecount, metadata, species) {
     #expression analysis
     dds <- DESeqDataSetFromMatrix(countData = countdata,
                                   colData = coldata,
-                                  design = ~Factor)
+                                  design = ~factor)
     keep <- rowSums(counts(dds)) >= 10
     dds <- dds[keep,]
     dds <- DESeq(dds)
@@ -194,7 +213,7 @@ vizexp <- function( genecount, metadata, species) {
     #pca plot
     output$pca_plot <- renderPlot({
       rld = rlog(dds)
-      plotPCA(rld, intgroup= "Factor")
+      plotPCA(rld, intgroup= "factor")
     })
     
     # volcano plot
@@ -203,7 +222,8 @@ vizexp <- function( genecount, metadata, species) {
       expression$diffexpressed[expression$log2FoldChange  > input$log  & expression$pvalue < input$pval ] <- "Upregulated genes"
       expression$diffexpressed[expression$log2FoldChange  < -(input$log)  & expression$pvalue < input$pval ] <- "Downregulated genes"
       #plot
-      ggplot(data=expression, aes(x=log2FoldChange, y=-log10(pvalue), col=diffexpressed)) + geom_point() + theme_minimal()
+      ggplot(data=expression, aes(x=log2FoldChange, y=-log10(pvalue), col=diffexpressed)) +
+        geom_point() + theme_minimal() + theme(text = element_text(size = 15))  
       
     })
     
@@ -247,7 +267,7 @@ vizexp <- function( genecount, metadata, species) {
     })
     
   }
-  # Run the application
-  shinyApp(ui = ui, server = server)
   
+  # run app
+  shinyApp(ui = ui, server = server)
 }
