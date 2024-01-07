4508fe2ca17dfd603807642987845acf2bede39a
ImraanDixon <106664513+ImraanDixon@users.noreply.github.com>
2024-01-07 22:41:24 +0100

    Update zzz.R

--- Package/R/zzz.R
+++ Package/R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function() {
   packageStartupMessage(paste0("This package was created with the Linux operating system in mind.", 
-                        "Functions callcnv(), callindels(), and callsnp() are nonfunctional on other operating systems."))
+                        "Functions callcnv(), callindels(), callsnp(), count(), and geneExpression() are nonfunctional on other operating systems."))
 }
