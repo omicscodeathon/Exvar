df89028aa8428fefdd78ac95766e94543a0b4d2b
ImraanDixon <106664513+ImraanDixon@users.noreply.github.com>
2024-01-07 22:44:21 +0100

    Update zzz.R

--- Package/R/zzz.R
+++ Package/R/zzz.R
@@ -1,4 +1,5 @@
 .onAttach <- function() {
   packageStartupMessage(paste0("This package was created with the Linux operating system in mind.", 
-                        "Functions callcnv(), callindels(), callsnp(), count(), and geneExpression() are nonfunctional on other operating systems."))
+                        "Functions callcnv(), callindels(), callsnp(), count(), and geneExpression() are nonfunctional on other operating systems.\n",
+                              "Additionally, a samtools installation is recommended for snp and indel calling."))
 }
